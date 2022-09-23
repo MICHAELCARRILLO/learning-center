418726a1085a3c9fc7e4556df1dec33aaa41d81c
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-09-23 14:29:34 -0500

    chore(UI): Added prime vue components.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css ";
 
 import "./assets/main.css"
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
 
 const app = createApp(App)
 
@@ -16,5 +20,11 @@ app.use(router)
 
 //using primevue
 app.use(PrimeVue)
+
+//defining prime vue components
+app.component("DataTable", DataTable);
+app.component("Column", Column);
+app.component("Toolbar", Toolbar);
+app.component("InputText", InputText);
 
 app.mount("#app")
