8427177792f771e116a307fc7fc828857c9ece74
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-09-23 14:25:08 -0500

    feat(UI): Added prime vue configuration.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
+import {createApp} from "vue"
+import App from "./App.vue"
+import router from "./router"
+import PrimeVue from "primevue/config";
 
-import './assets/main.css'
+//primevue theme
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css ";
+
+import "./assets/main.css"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+//using primevue
+app.use(PrimeVue)
+
+app.mount("#app")
