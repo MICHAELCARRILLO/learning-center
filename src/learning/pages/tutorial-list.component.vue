af44a10a8602203650313e767adf1307ea189d80
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-09-24 09:36:53 -0500

    feat(ui): Added remaining ui components and PrimeVue style companion.

--- src/learning/pages/tutorial-list.component.vue
+++ src/learning/pages/tutorial-list.component.vue
@@ -4,9 +4,10 @@
 
 <script>
 import {TutorialsApiService} from "@/learning/services/tutorials-api.service";
+import {FilterMatchMode} from "primevue/api";
 
 export default {
-  name: "tutorials",
+  name: "tutorial-list",
   data() {
     return {
       tutorials: [],
@@ -18,14 +19,40 @@ export default {
       filters: {},
       submitted: false,
       statuses: [
-        {label: "Published", value: true},
-        {label: "Unpublished", value: false},
+        {label: "Published", value: "published"},
+        {label: "Unpublished", value: "unpublished"},
       ],
-      tutorialService: new TutorialsApiService(),
+      tutorialService: null,
     }
   },
-
+  //it initializes everything
+  created() {
+    this.tutorialService = new TutorialsApiService();
+    this.tutorialService.getAll()
+        .then((response) =>{
+          this.tutorials = response.data;
+          console.log(this.tutorials);
+          this.tutorials.forEach((tutorial)=>
+            this.getDisplayableTutorial(tutorial));
+          console.log(this.tutorials);
+        });
+        this.initFilters();
+    },
   methods: {
+    getDisplayableTutorial(tutorial){
+      tutorial.status = tutorial.published
+      ? this.statuses[0].label
+          : this.statuses[1].label;
+      return tutorial;
+    },
+    getStorableTutorial(displayableTutorial){
+        return {
+          id: displayableTutorial.id,
+          title: displayableTutorial.title,
+          description: displayableTutorial.description,
+          published : displayableTutorial.status.label === "Published",
+        };
+    },
     openNew() {
       this.tutorial = {};
       this.submitted = false;
@@ -36,7 +63,7 @@ export default {
       this.submitted = false;
     },
     findIndexById(id) {
-      return this.tutorial.findIndex((tutorial) => tutorial.id == id);
+      return this.tutorial.findIndex((tutorial) => tutorial.id === id);
     },
     saveTutorial() {
       this.submitted = true;
@@ -45,7 +72,7 @@ export default {
         if (this.tutorial.id) {
           this.tutorialService.update(this.tutorial.id, this.tutorial)
               .then((response) => {
-                this.tutorials[this.tutorial.findIndex(this.tutorial.id)] = this.tutorial;
+                this.tutorials[this.tutorial.findIndex(response.id)] = this.getDisplayableTutorial(response.data);
                 this.$toast.add({
                   severity: "success",
                   summary: "Successful",
@@ -60,7 +87,7 @@ export default {
           this.tutorial.id = 0;
           this.tutorial.create(this.tutorial)
               .then((response) => {
-                this.tutorial.id = response.data.id;
+                this.tutorial = this.getDisplayableTutorial(response.data);
                 this.tutorials.push(this.tutorial);
                 this.$toast.add({
                   severity: "success",
@@ -99,9 +126,34 @@ export default {
             });
             console.log(response);
           });
+    },
+    exportToCSV(){
+      this.$refs.dt.exportToCSV();
+    },
+    confirmDeleteSelected(){
+        this.deleteTutorialDialog = true;
+    },
+    deleteSelectedTutorials(){
+      this.selectTutorials.forEach((tutorial)=>{
+          this.tutorialService.delete(tutorial.id)
+              .then((response)=>{
+              this.tutorials = this.tutorials.filter(
+                  (t) => t.id !== this.tutorial.id
+              );
+              console.log(response);
+          } );
+      } );
+      this.deleteTutorialDialog = false;
+    },
+
+    initFilters(){
+      this.filters = {
+        global: {value: null, matchMode: FilterMatchMode.CONTAINS}
+      }
     }
-  }
-}
+
+  },
+};
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,53 @@
-import {createApp} from "vue"
-import App from "./App.vue"
-import router from "./router"
+import {createApp} from "vue";
+import App from "./App.vue";
+import router from "./router";
 import PrimeVue from "primevue/config";
 
 //primevue theme
 import "primevue/resources/themes/md-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css ";
+import  "primeflex/primeflex.css";
 
-import "./assets/main.css"
+import "./assets/main.css";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Toolbar from "primevue/toolbar";
 import InputText from "primevue/inputtext";
+import ToastService from "primevue/toastservice";
+import Button from "primevue/button";
+import Row from "primevue/row";
+import Sidebar from "primevue/sidebar";
+import Menu from "primevue/menu";
+import Dialog from "primevue/dialog";
+import Toast from "primevue/toast";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
+import Card from "primevue/card";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
+
+//implementando Toast Para los mensages
+app.use(ToastService);
 
 //using primevue
-app.use(PrimeVue)
+app.use(PrimeVue, {ripple: true});
 
 //defining prime vue components
-app.component("DataTable", DataTable);
-app.component("Column", Column);
-app.component("Toolbar", Toolbar);
-app.component("InputText", InputText);
+app.component("pv-data-table", DataTable);
+app.component("pv-column", Column);
+app.component("pv-toolbar", Toolbar);
+app.component("pv-input-text", InputText);
+app.component("pv-button", Button);
+app.component("pv-row", Row);
+app.component("pv-sidebar", Sidebar);
+app.component("pv-menu", Menu);
+app.component("pv-dialog", Dialog);
+app.component("pv-toast", Toast);
+app.component("pv-dropdown", Dropdown);
+app.component("pv-tag", Tag);
+app.component("pv-card", Card);
 
-app.mount("#app")
+app.mount("#app");
